8c7cf4f439eda1ec4f5cb33fab6e84cd749f9971
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-05-01 20:27:37 -0500

    Mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:codigo4_states2/person.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(MyApp());
@@ -45,6 +46,18 @@ class _HomePageState extends State<HomePage> {
     ),
   ];
 
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,32 +81,45 @@ class _HomePageState extends State<HomePage> {
           "Listado general",
         ),
       ),
-      // body: Column(
-      //   children: [
-      //     Text("Inicio de la lista"),
-      //     Text("Inicio de la lista"),
-      //     Text("Inicio de la lista"),
-      //     ...List.generate(
-      //       nombres.length,
-      //       (int index) => Text(
-      //         nombres[index],
-      //       ),
-      //     ),
-      //     Text("Fin de la lista"),
-      //   ],
-      // ),
       body: Column(
-        children: people2
-            .map(
-              (Person e) => ListTile(
-                title: Text(e.name),
-                subtitle: Text(e.address),
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(e.image),
+        children: [
+          CarouselSlider(
+            items: imgList.map((e) => Image.network(e)).toList(),
+            // items: [
+            //   Text("Hola"),
+            //   Text("Hola 2"),
+            //   Text("Hola 3"),
+            //   Text("Hola 4"),
+            // ],
+            carouselController: _controller,
+            options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: false,
+                aspectRatio: 1,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == entry.key ? Colors.red : Colors.black,
+                  ),
                 ),
-              ),
-            )
-            .toList(),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
